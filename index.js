9b1ad93baadc3e6d45e665d9d939b8dffd66db0a
vaish0207 <vaishu0299@gmail.com>
2022-09-28 16:13:57 -0400

    changed api name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || "8000";
 
-app.get("/healthCheck", (req, res) => {
+app.get("/healthz", (req, res) => {
     res.status(200).send({"statusCode":200, "message":"healthCheck successful!!!"});
 });
 
