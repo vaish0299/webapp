ae74f3da9e9d747dfc362e14ab6a29a146287b8a
vaish0207 <vaishu0299@gmail.com>
2022-10-05 03:41:11 -0400

    Add mysql connection support

---
+++ src/utils/database.js
@@ -0,0 +1,19 @@
+const config = require('../../db-config.json');
+const mysql = require('mysql2/promise');
+const { Sequelize } = require('sequelize');
+
+module.exports = db = {};
+
+initialize();
+
+async function initialize() {
+    const { host, port, user, password, database } = config.database;
+    const connection = await mysql.createConnection({ host, port, user, password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql', logging:false });
+
+    db.User = require('../user/user-model')(sequelize);
+
+    await sequelize.sync();
+}
