c1eff280fd4d28b08ce711448a47bfa4b3a0aee1
vaish0207 <vaishu0299@gmail.com>
2022-10-18 14:13:09 -0400

    test build image

---
+++ packer_init.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+echo_info () {
+    echo $1
+}
+
+# Updating packages
+echo_info UPDATES-BEING-INSTALLED
+sudo apt-get update
+
+# Setting up the cli
+echo_info PATH-SET-LINUX
+PATH=/usr/bin:/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/local/bin:/opt/aws/bin:/root/bin
+
+#installing node server
+sudo apt-get install curl
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+sudo apt-get install nodejs
+
+#installing mysql server
+sudo apt install mysql-server
+sudo systemctl start mysql.service
+sudo mysql
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+exit
+systemctl status mysql.service
+
+sudo npm install -g pm2
+
+mkdir /home/ubuntu/webapp
+chown ubuntu:ubuntu /home/ubuntu/webapp
+
+cd /home/ubuntu/webapp
+sudo npm install
+sudo pm2 start server.js
+sudo pm2 startup systemd
+sudo pm2 save
+sudo ln -s /home/ubuntu/webapp/node-service.service /etc/systemd/system/node-service.service
+sudo systemctl daemon-reload
+sudo systemctl enable node-service.service
+sudo systemctl start node-service.service
+ 
