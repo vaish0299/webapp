e8cf7191043a29633e04a5204d191f117f9146c2
vaish0207 <vaishu0299@gmail.com>
2022-11-08 12:46:24 -0500

    added statsDincrs

--- src/document/document-controller.js
+++ src/document/document-controller.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const validateRequest = require('../middleware/validate');
 const authorize = require('../middleware/basic-auth')
 const documentService = require('./document-service');
-//const statsdClient= require("../utils/statsdUtil");
+const statsdClient= require("../utils/statsdUtil");
 // routes
 router.post('/documents', authorize, documentValidation, upload);
 router.get('/documents', authorize, getAllDocuments);
@@ -23,28 +23,28 @@ function documentValidation(req, res, next) {
 }
 
 function upload(req, res, next) {
-   // statsdClient.increment('post_/self');
+    statsdClient.increment('post_/self');
     documentService.upload(req)
         .then(data => res.status(201).json(data))
         .catch(next);
 }
 
 function getAllDocuments(req, res, next) {
-   // statsdClient.increment('get_/self');
+    statsdClient.increment('get_/self');
     documentService.getAllDocs(req)
         .then(data => res.status(200).json(data))
         .catch(next);
 }
 
 function getDocumentById(req, res, next) {
-   // statsdClient.increment('get_/self');
+    statsdClient.increment('get_/self');
     documentService.getById(req, req.params.doc_id)
         .then(user => res.json(user))
         .catch(next);
 }
 
 function deleteDocument(req, res, next) {
-   // statsdClient.increment('delete_/self');
+    statsdClient.increment('delete_/self');
     documentService.deleteDoc(req.params.doc_id, req)
         .then(user => res.status(204).json(user))
         .catch(next);
