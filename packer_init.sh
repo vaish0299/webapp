dc6db0a0b2238fa69a7020e4942187011c0799c1
vaish0207 <vaishu0299@gmail.com>
2022-11-08 17:20:25 -0500

    commit

--- packer_init.sh
+++ packer_init.sh
@@ -28,6 +28,7 @@ sudo apt install wget
 
 echo_info INSTALLING-CLOUDWATCH-AGENT
 wget https://s3.us-east-1.amazonaws.com/amazoncloudwatch-agent-us-east-1/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb 
+
 echo_info INSTALLING_CLOUDWATCH_AGENT
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 echo_info CONFIGURING_CLOUDWATCH_AGENT
