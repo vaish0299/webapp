3a960dfbd1d132be81b50e466cb85c60aba5414b
vaish0207 <vaishu0299@gmail.com>
2022-11-09 21:23:24 -0500

    commit

--- src/mylog/logger.js
+++ src/mylog/logger.js
@@ -1,8 +1,9 @@
-const { createLogger, format, transports } = require('winston')
+const { createLogger, format, transports } = require("winston");
+const winstonCloudWatch = require("winston-aws-cloudwatch");
 // const moment = require('moment')
 
 // set default log level.
-const logLevel = 'info'
+const logLevel = "info";
 
 var logger = createLogger({
   level: logLevel,
@@ -12,36 +13,50 @@ var logger = createLogger({
     warn: 2,
     info: 3,
     debug: 4,
-    trace: 5
+    trace: 5,
   },
   format: format.combine(
     format.prettyPrint(),
     format.timestamp({
-      format: 'DD-MM-YYYY hh:mm:ss A'
+      format: "DD-MM-YYYY hh:mm:ss A",
     }),
-    format.printf(nfo => {
-      return `${nfo.timestamp} - ${nfo.level}: ${nfo.message}`
+    format.printf((nfo) => {
+      return `${nfo.timestamp} - ${nfo.level}: ${nfo.message}`;
     })
   ),
   transports: [
     new transports.Console(),
     new transports.File({
-      filename: 'bot.log'
+      filename: "bot.log",
+    }),
+  ],
+});
+//if (process.env.NODE_ENV === "production") {
+  logger.add(
+    new winstonCloudWatch({
+      logGroupName: "csye6225",
+      logStreamName: "webapp",
+      createLogGroup: true,
+      createLogStream: true,
+      awsConfig: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: "us-east-1",
+      },
     })
-  ]
-})
-
+  );
+//}
 // Extend logger object to properly log 'Error' types
-var origLog = logger.log
+var origLog = logger.log;
 
 logger.log = function (level, msg) {
   if (msg instanceof Error) {
-    var args = Array.prototype.slice.call(arguments)
-    args[1] = msg.stack
-    origLog.apply(logger, args)
+    var args = Array.prototype.slice.call(arguments);
+    args[1] = msg.stack;
+    origLog.apply(logger, args);
   } else {
-    origLog.apply(logger, arguments)
+    origLog.apply(logger, arguments);
   }
-}
+};
 
-module.exports = logger
+module.exports = logger;
