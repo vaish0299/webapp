8eecce9d9771e247f7a5b94efdcbae5a3eb87f90
vaish0207 <vaishu0299@gmail.com>
2022-12-01 02:24:18 -0500

    commit

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/health", (req, res) => {
+app.get("/healthz", (req, res) => {
     res.status(200).send({"statusCode":200, "message":"healthCheck successful!!!"});
 });
 
